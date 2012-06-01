adbca22c295dd3f80421d3e58d1198e30a3360d0
Rajendra Nayak <rnayak@ti.com>
2012-06-01 14:02:49 +0530

    ARM: omap: clk: Define a function to enable clocks at init
    Platform code can use omap2_clk_enable_init_clocks() to enable a
    list of clocks that are needed to be enabled at init.
    
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>

--- arch/arm/mach-omap2/clock.c
+++ arch/arm/mach-omap2/clock.c
@@ -391,6 +391,17 @@ int omap2_clk_disable_autoidle_all(void)
 	return 0;
 }
 
+void omap2_clk_enable_init_clocks(const char **clk_names, u8 num_clocks)
+{
+	struct clk *init_clk;
+	int i;
+
+	for (i = 0; i < num_clocks; i++) {
+		init_clk = clk_get(NULL, clk_names[i]);
+		clk_prepare_enable(init_clk);
+	}
+}
+
 const struct clk_hw_omap_ops clkhwops_wait = {
 	.find_idlest	= omap2_clk_dflt_find_idlest,
 	.find_companion	= omap2_clk_dflt_find_companion,
