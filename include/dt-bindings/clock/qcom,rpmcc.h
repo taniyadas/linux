af4a37e6d38452a65ac3b034557effc6ece35e9a
Bjorn Andersson <bjorn.andersson@sonymobile.com>
2015-12-10 16:00:38 -0800

    clk: qcom: rpmcc: Add 8974 clocks
    Signed-off-by: Bjorn Andersson <bjorn.andersson@sonymobile.com>

--- include/dt-bindings/clock/qcom,rpmcc.h
+++ include/dt-bindings/clock/qcom,rpmcc.h
@@ -42,4 +42,43 @@
 #define RPM_RF_CLK2_PIN				24
 #define RPM_RF_CLK2_A_PIN			25
 
+#define RPM_PNOC_CLK				26
+#define RPM_PNOC_A_CLK				27
+#define RPM_CNOC_CLK				30
+#define RPM_CNOC_A_CLK				31
+#define RPM_MMSSNOC_AHB_CLK			32
+#define RPM_MMSSNOC_AHB_A_CLK			33
+#define RPM_GFX3D_CLK_SRC			36
+#define RPM_GFX3D_A_CLK_SRC			37
+#define RPM_OCMEMGX_CLK				38
+#define RPM_OCMEMGX_A_CLK			39
+#define RPM_CXO_CLK_SRC				40
+#define RPM_CXO_A_CLK_SRC			41
+#define RPM_CXO_D0				44
+#define RPM_CXO_D0_A				45
+#define RPM_CXO_D1				46
+#define RPM_CXO_D1_A				47
+#define RPM_CXO_A0				48
+#define RPM_CXO_A0_A				49
+#define RPM_CXO_A1				50
+#define RPM_CXO_A1_A				51
+#define RPM_CXO_A2				52
+#define RPM_CXO_A2_A				53
+#define RPM_DIV_CLK1				54
+#define RPM_DIV_A_CLK1				55
+#define RPM_DIV_CLK2				56
+#define RPM_DIV_A_CLK2				57
+#define RPM_DIFF_CLK				58
+#define RPM_DIFF_A_CLK				59
+#define RPM_CXO_D0_PIN				60
+#define RPM_CXO_D0_A_PIN			61
+#define RPM_CXO_D1_PIN				62
+#define RPM_CXO_D1_A_PIN			63
+#define RPM_CXO_A0_PIN				64
+#define RPM_CXO_A0_A_PIN			65
+#define RPM_CXO_A1_PIN				66
+#define RPM_CXO_A1_A_PIN			67
+#define RPM_CXO_A2_PIN				68
+#define RPM_CXO_A2_A_PIN			69
+
 #endif
