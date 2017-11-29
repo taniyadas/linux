b1a3da22799e265e7d583e5a237a703157f19d67
Viresh Kumar <viresh.kumar@linaro.org>
2017-11-29 15:18:36 +0530

    PM / OPP: Implement of_dev_pm_opp_find_required_opp()
    A device's DT node or its OPP nodes can contain a phandle to other
    device's OPP node, in the "required-opp" property.
    
    This patch implements a routine to find that required OPP from the node
    that contains the "required-opp" property.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/opp/opp.h
+++ drivers/opp/opp.h
@@ -187,6 +187,7 @@ struct opp_table {
 };
 
 /* Routines internal to opp core */
+void dev_pm_opp_get(struct dev_pm_opp *opp);
 void _get_opp_table_kref(struct opp_table *opp_table);
 struct opp_table *_find_opp_table(struct device *dev);
 struct opp_device *_add_opp_dev(const struct device *dev, struct opp_table *opp_table);
