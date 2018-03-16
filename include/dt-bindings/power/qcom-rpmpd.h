c0d05d008bc7fdf27fcc3f25e19047bc0d6df4ff
Rajendra Nayak <rnayak@codeaurora.org>
2018-03-16 09:38:19 +0530

    soc: qcom: rpmpd: Add a Power domain driver to model corners
    The Power domains for corners just pass the performance state set by the
    consumers to the RPM (Remote Power manager) which then takes care
    of setting the appropriate voltage on the corresponding rails to
    meet the performance needs.
    
    We add all Power domain data needed on msm8996 here. This driver can easily
    be extended by adding data for other qualcomm SoCs as well.
    
    Signed-off-by: Rajendra Nayak <rnayak@codeaurora.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

---
+++ include/dt-bindings/power/qcom-rpmpd.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2018, The Linux Foundation. All rights reserved. */
+
+#ifndef _DT_BINDINGS_POWER_QCOM_RPMPD_H
+#define _DT_BINDINGS_POWER_QCOM_RPMPD_H
+
+/* MSM8996 Power Domain Indexes */
+#define MSM8996_VDDCX		0
+#define MSM8996_VDDCX_AO	1
+#define MSM8996_VDDCX_VFC	2
+#define MSM8996_VDDMX		3
+#define MSM8996_VDDMX_AO	4
+#define MSM8996_VDDSSCX		5
+#define MSM8996_VDDSSCX_VFC	6
+
+#endif
